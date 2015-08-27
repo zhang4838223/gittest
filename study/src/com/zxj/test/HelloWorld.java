cb28399d849fc441be31f41dbff9202c9b332fac
unknown <xiaojun.zhang@PC-zhangxiaojun.morefun.com>
2015-08-27 14:43:29 +0800

    Initial commit

---
+++ study/src/com/zxj/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.zxj.test;
+
+/**
+ * Created by xiaojun.zhang on 2015/7/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
